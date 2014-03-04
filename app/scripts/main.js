e854a49695e56f4aa615778ee66490820646a648
Steve Kinney <hello@stevekinney.net>
2014-03-04 11:12:40 -0500

    Pull survey data into the code itself

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,23 +6,15 @@ EarningCredit.loadView = function (view, next) {
 };
 
 EarningCredit.init = function () {
-  $.getJSON('./surveys/pre-survey.json').done(function (response) {
-    EarningCredit.preSurvey = new EarningCredit.Survey(response);
-    
-    $.getJSON('./surveys/post-survey.json').done(function (response) {
-      EarningCredit.postSurvey = new EarningCredit.Survey(response);
-      
-      EarningCredit.loadView(new EarningCredit.IntroductionView(), function () {
-        var survey = new EarningCredit.SurveyView({ collection: EarningCredit.preSurvey });
+  EarningCredit.loadView(new EarningCredit.IntroductionView(), function () {
+    var survey = new EarningCredit.SurveyView({ collection: EarningCredit.preSurvey });
+    EarningCredit.loadView(survey, function () {
+      var view = new EarningCredit.LoanSelectionView();
+      EarningCredit.loadView(view, function () {
+        var survey = new EarningCredit.SurveyView({ collection: EarningCredit.postSurvey });
         EarningCredit.loadView(survey, function () {
-          var view = new EarningCredit.LoanSelectionView();
-          EarningCredit.loadView(view, function () {
-            var survey = new EarningCredit.SurveyView({ collection: EarningCredit.postSurvey });
-            EarningCredit.loadView(survey, function () {
-              var view = new EarningCredit.ConclusionView();
-              EarningCredit.loadView(view);
-            });
-          });
+          var view = new EarningCredit.ConclusionView();
+          EarningCredit.loadView(view);
         });
       });
     });
